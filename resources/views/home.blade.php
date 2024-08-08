7e168d72836ecbc7b253574bf7e1e8d002fb8085
silvianainggolan <silvianainggolan72@gmail.com>
2024-08-08 09:31:19 +0700

    update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HOME</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+        <div class="container "></div>
+    
+  <ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" href="user">TAMU</a>
+  </li>
+  
+  <li class="nav-item">
+  <a class="nav-link active" href="admin">Admin</a>
+    
+  </li>
+  </nav>
+</ul>
+  <div class="container mt-4">
+    <h1>BUKU TAMU</h1>
+    <H2>KOMINFO KAB.DAIRI</H2>
+  </div>
+
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+
+
