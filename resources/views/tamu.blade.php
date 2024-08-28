aa31495771cef0a441c40d900cf1ca37a696eedd
alexpanjaitan26 <alexanderpanjaitan26@gmail.com>
2024-08-28 19:59:10 +0700

    update tamu

--- resources/views/tamu.blade.php
+++ resources/views/tamu.blade.php
@@ -21,7 +21,6 @@
                             type="text"
                             placeholder="Cari berdasarkan nama, no handphone, email..."
                             value="{{ request('search') }}"
-                            style="color: black;" 
                             class="flex-1 mr-2 border border-gray-300 rounded px-3 py-2"
                         />
                         <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded">
@@ -29,64 +28,67 @@
                         </button>
                     </form>
 
-                    <!-- Tabel Tamu -->
-                    <x-bladewind::table>
-                        <x-slot name="header">
-                            <th>No</th>
-                            <th>Status</th>
-                            <th>Nama</th>
-                            <th>No Handphone</th>
-                            <th>Email</th>
-                            <th>Keperluan</th>
-                            <th>pegawai</th>
-                            <th>tanggal / waktu permohonan</th>
-                            <th>tanggal / waktu berkunjung</th>
-                        </x-slot>
-                        
-                        @forelse ($tamu as $item)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>
-                                    @if ($item->status == 0)
-                                        <x-bladewind::tag label="Belum Dikonfirmasi" color="red" />    
-                                    @else
-                                        <x-bladewind::tag label="Dikonfirmasi" color="green" />
-                                    @endif
+                    <!-- Tabel Tamu dengan Scroll Horizontal -->
+                    <div class="overflow-x-auto mt-6">
+                        <x-bladewind::table class="min-w-full">
+                            <x-slot name="header">
+                                <th>No</th>
+                                <th>Status</th>
+                                <th>Nama</th>
+                                <th>No Handphone</th>
+                                <th>Email</th>
+                                <th>Keperluan</th>
+                                <th>pegawai</th>
+                                <th>tanggal / waktu permohonan</th>
+                                <th>tanggal / waktu berkunjung</th>
+                                <th>Aksi</th>
+                            </x-slot>
+                            
+                            @forelse ($tamu as $item)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        @if ($item->status == 0)
+                                            <x-bladewind::tag label="Belum Dikonfirmasi" color="red" />    
+                                        @else
+                                            <x-bladewind::tag label="Dikonfirmasi" color="green" />
+                                        @endif
 
-                                    @if (isset($item->pesan))
-                                        <br>
-                                        <small>{{ $item->pesan }}</small>
-                                    @endif
-                                </td>
-                                <td>{{ $item->nama }}</td>
-                                <td>{{ $item->nomor_handphone }}</td>
-                                <td>{{ $item->email }}</td>
-                                <td>{{ $item->keperluan }}</td>
-                                <td>{{ $item->nip }}<br>{{$item->pegawai->nama}} </td> 
-                                <td>{{ $item->tanggal_berkunjung}} <br>{{ $item->jam_berkunjung}}</td>
-                                <td>{{ $item->tanggal_konfirmasi }} <br>{{ $item->waktu_konfirmasi }}</td>
-                                <td>{{ $item->tahun->nama ?? 'N/A' }}</td>
-                                <td>
-                                    <a href="{{ route('tamu.konfirmasi', $item->id) }}" class="btn btn-sm btn-primary">
-                                        <x-bladewind::button color="green">KONFIRMASI</x-bladewind::button>
-                                    </a>
-                                    <a href="{{ route('tamu.edit', $item->id) }}" class="btn btn-sm btn-primary">
-                                        <x-bladewind::button color="yellow">EDIT</x-bladewind::button>
-                                    </a>
-                                    <!-- Custom Delete Form -->
-                                    <form action="{{ route('tamu.hapus', $item->id) }}" method="POST" style="display:inline-block;" class="delete-form">
-                                        @csrf
-                                        @method('DELETE')
-                                        <x-bladewind::button color="red" class="delete-btn" data-id="{{ $item->id }}">HAPUS</x-bladewind::button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="8" class="text-center">Tidak ada data yang ditemukan.</td>
-                            </tr>
-                        @endforelse
-                    </x-bladewind::table>
+                                        @if (isset($item->pesan))
+                                            <br>
+                                            <small>{{ $item->pesan }}</small>
+                                        @endif
+                                    </td>
+                                    <td>{{ $item->nama }}</td>
+                                    <td>{{ $item->nomor_handphone }}</td>
+                                    <td>{{ $item->email }}</td>
+                                    <td>{{ $item->keperluan }}</td>
+                                    <td>{{ $item->nip }}<br>{{$item->pegawai->nama}} </td> 
+                                    <td>{{ $item->tanggal_berkunjung}} <br>{{ $item->jam_berkunjung}}</td>
+                                    <td>{{ $item->tanggal_konfirmasi }} <br>{{ $item->waktu_konfirmasi }}</td>
+                                    <td class="whitespace-nowrap">
+                                        <div class="flex space-x-2">
+                                            <a href="{{ route('tamu.konfirmasi', $item->id) }}">
+                                                <x-bladewind::button color="green" class="hover:shadow-md">KONFIRMASI</x-bladewind::button>
+                                            </a>
+                                            <a href="{{ route('tamu.edit', $item->id) }}">
+                                                <x-bladewind::button color="yellow" class="hover:shadow-md">EDIT</x-bladewind::button>
+                                            </a>
+                                            <form action="{{ route('tamu.hapus', $item->id) }}" method="POST" style="display:inline-block;" class="delete-form">
+                                                @csrf
+                                                @method('DELETE')
+                                                <x-bladewind::button color="red" class="delete-btn hover:shadow-md" data-id="{{ $item->id }}">HAPUS</x-bladewind::button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="10" class="text-center">Tidak ada data yang ditemukan.</td>
+                                </tr>
+                            @endforelse
+                        </x-bladewind::table>
+                    </div>
 
                     <!-- Pagination Links -->
                     <div class="mt-4">
